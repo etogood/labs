cbfa34fd1c4af565832e6593a7ea9739b451f8de
Ilya <shaura@asv.me>
2024-12-06 01:40:22 +0300

    asoiu

--- ООП/OOP/Shaura.OOP/Shaura.OOP.Lab1/obj/Debug/net8.0/Shaura.OOP.Lab1.AssemblyInfo.cs
+++ ООП/OOP/Shaura.OOP/Shaura.OOP.Lab1/obj/Debug/net8.0/Shaura.OOP.Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shaura.OOP.Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a42241b7cef49587d702f98e7c89a0fb80bebc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shaura.OOP.Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shaura.OOP.Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
